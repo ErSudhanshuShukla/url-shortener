89e67c9f6f957d2dadeea432ba57feeecd9c95ab
Sudhanshu Shukla <er.sudhanshushukla@gmail.com>
2026-09-16 14:52:16 +0530

    chore: add production start script

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,6 @@ import config from "./config/config.js";
 
 await connectDB();
 
-app.listen(config.PORT, () => {
+app.listen(config.PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${config.PORT}`);
 });
