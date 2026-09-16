4cf117d4eda4a452b28b1259ebac473b00022945
Sudhanshu Shukla <er.sudhanshushukla@gmail.com>
2026-09-16 15:48:20 +0530

    fix: use environment port for deployment

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 
 const config = {
   MONGODB_URI: process.env.MONGODB_URI,
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 3000,
   BASE_URL: process.env.BASE_URL,
   CORS_ORIGIN: process.env.CORS_ORIGIN,
 };
